940f015047d5cc3927ed725dec48c453bbe3e4b5
“aniekanmark55” <aniekanmark55@gmail.com>
2023-04-21 22:05:21 -0700

    config

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,10 @@
-# create a manifest that kills a process named killmenow
-# using pkill with puppet
-
-exec { 'pkill':
-  command => 'pkill -f killmenow',
-  path    => '/usr/bin',
+# Using Puppet, create a manifest that kills a process named killmenow.
+# Requirements:
+# Must use the exec Puppet resource
+# Must use pkill
+exec { 'killing a process using pkill':
+  command  => 'pkill -9 killmenow',
+  path     => '/usr/bin:/bin',
+  onlyif   => 'pgrep killmenow',
+  provider => shell,
 } 
